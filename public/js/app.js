d0796668032e31ffd6fb8e5b17c1fa0484cbda08
Chido Ukaigwe <chidodesigns@gmail.com>
2020-10-27 21:12:45 +0000

    Front End User Profile View Complete

--- public/js/app.js
+++ public/js/app.js
@@ -2201,6 +2201,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_Post__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../components/Post */ "./resources/js/components/Post.vue");
 //
 //
 //
@@ -2209,12 +2210,34 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "Show",
+  components: {
+    Post: _components_Post__WEBPACK_IMPORTED_MODULE_0__["default"]
+  },
   data: function data() {
     return {
       user: null,
-      loading: true
+      posts: null,
+      userLoading: true,
+      postLoading: true
     };
   },
   mounted: function mounted() {
@@ -2226,14 +2249,14 @@ __webpack_require__.r(__webpack_exports__);
     })["catch"](function (error) {
       console.log('Unable to fetch the user from the server');
     })["finally"](function () {
-      _this.loading = false;
+      _this.userLoading = false;
     });
-    axios.get('/api/posts/' + this.$route.params.userId).then(function (res) {
+    axios.get('/api/users/' + this.$route.params.userId + '/posts').then(function (res) {
       _this.posts = res.data;
-      _this.loading = false;
     })["catch"](function (error) {
       console.log('Unable to fetch posts');
-      _this.loading = false;
+    })["finally"](function () {
+      _this.postLoading = false;
     });
   }
 });
@@ -20469,24 +20492,71 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    { staticClass: "flex flex-col items-center" },
+    [
+      _c("div", { staticClass: "relative mb-8" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c(
+          "div",
+          {
+            staticClass:
+              "absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20"
+          },
+          [
+            _vm._m(1),
+            _vm._v(" "),
+            _c("p", { staticClass: "text-2xl ml-4 text-gray-100" }, [
+              _vm._v(_vm._s(_vm.user.data.attributes.name))
+            ])
+          ]
+        )
+      ]),
+      _vm._v(" "),
+      _vm.postLoading
+        ? _c("p", [_vm._v("Loading posts...")])
+        : _vm._l(_vm.posts.data, function(post) {
+            return _c("Post", { key: post.data.post_id, attrs: { post: post } })
+          }),
+      _vm._v(" "),
+      !_vm.postLoading && _vm.posts.data.length < 1
+        ? _c("p", [_vm._v("No posts found. Get Started!")])
+        : _vm._e()
+    ],
+    2
+  )
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [
-      _c("div", { staticClass: "w-100 h-64 overflow-hidden" }, [
-        _c("img", {
-          staticClass: "object-cover w-full",
-          attrs: {
-            src:
-              "https://images.pexels.com/photos/2788430/pexels-photo-2788430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            alt: "User Background Image"
-          }
-        })
-      ])
+    return _c("div", { staticClass: "w-100 h-64 overflow-hidden z-10" }, [
+      _c("img", {
+        staticClass: "object-cover w-full",
+        attrs: {
+          src:
+            "https://images.pexels.com/photos/2788430/pexels-photo-2788430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          alt: "User Background Image"
+        }
+      })
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "w-32" }, [
+      _c("img", {
+        staticClass:
+          "object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg",
+        attrs: {
+          src: "https://chido-designs.co.uk/images/main/Chido.jpg",
+          alt: "User Profile Image"
+        }
+      })
     ])
   }
 ]
--- resources/js/views/Users/Show.vue
+++ resources/js/views/Users/Show.vue
@@ -1,19 +1,45 @@
 <template>
-    <div>
-        <div class="w-100 h-64 overflow-hidden">
-            <img src="https://images.pexels.com/photos/2788430/pexels-photo-2788430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="User Background Image" class="object-cover w-full">
+    <div class="flex flex-col items-center">
+        <div class="relative mb-8">
+
+            <div class="w-100 h-64 overflow-hidden z-10">
+                <img src="https://images.pexels.com/photos/2788430/pexels-photo-2788430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="User Background Image" class="object-cover w-full">
+            </div>
+
+            <div class="absolute flex items-center bottom-0 left-0 -mb-8 ml-12 z-20">
+                <div class="w-32">
+                    <img src="https://chido-designs.co.uk/images/main/Chido.jpg" alt="User Profile Image" class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg">
+                </div>
+                <p class="text-2xl ml-4 text-gray-100">{{user.data.attributes.name}}</p>
+            </div>
         </div>
+         <p v-if="postLoading">Loading posts...</p>
+        <!-- We are passing down/through the :post to the Post.vue component-->
+        <Post v-else v-for="post in posts.data" :key="post.data.post_id" :post="post"/>
+
+        <p v-if="!postLoading && posts.data.length < 1">No posts found. Get Started!</p>
+
     </div>
 </template>
 
 <script>
+
+    import Post from '../../components/Post';
+
     export default {
         name: "Show",
+
+        components: {
+            Post,
+        },
 
         data: () => {
             return {
                 user: null,
-                loading: true
+                posts: null,
+                userLoading: true,
+                postLoading: true
+
             }
         },
 
@@ -29,18 +55,19 @@
 
                 })
                 .finally(() => {
-                    this.loading = false;
+                    this.userLoading = false;
                 });
 
 
-            axios.get('/api/posts/' + this.$route.params.userId)
+            axios.get('/api/users/' + this.$route.params.userId + '/posts')
                 .then(res => {
                     this.posts = res.data;
-                    this.loading = false;
                 })
                 .catch(error => {
                     console.log('Unable to fetch posts');
-                    this.loading = false;
+                })
+                .finally(() => {
+                    this.postLoading = false;
                 });
 
         }
